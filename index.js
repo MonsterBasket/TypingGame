68ef49d7e5daeefb2e9a4e5a72f9a5188f4d0423
MonsterBasket <blazza1@hotmail.com>
2022-08-01 00:16:30 +1000

    Got my dynamic background working!
    That's the first step in allowing dynamic, user selectable themes

--- index.js
+++ index.js
@@ -25,6 +25,46 @@
 // - user can't die on this stage (game is essentially paused) and there is no scoring.  Type the name of the
 // - theme to choose, this will trigger a new background image and word list.
 
-// Unsplash Access key: etHez9ur_31n4YuNvHsHVpUzp9btB4v-C7JiGbtEBG8
-// Unsplash Secret key: Zkknvgu1ZE5fbtBBCHLVVL9G0AxRL9JXLwr02ng3RTQ
-// These are supposed to stay confidential somehow?
+
+
+//this works!  Sizing is not working from the request, but I'm brute forcing it with css.
+//If I could get a bunch of random words related to a search term, the user could just type their own themes!
+//Need to add who the photo is by and allow user to download it if they want (unsplash terms)
+function newBackground(theme){
+    return fetch(`https://api.unsplash.com/photos/random?query=${theme}&w=1200&h=600`, {
+        headers: {
+            "Authorization": "Client-ID etHez9ur_31n4YuNvHsHVpUzp9btB4v-C7JiGbtEBG8"
+        }
+    })
+    .then(r => r.json())
+    .then(j => {console.log(j);
+        document.querySelector("#game").style.backgroundImage = `url(${j.urls.full})`;
+        //document.querySelector("#game").style.backgroundSize = "1200px 600px";
+    })
+    .catch(err => console.log("what's going on here?", err));
+}
+newBackground("desert");
+
+function loadScores() {
+    return fetch("./db.json")
+        .then(resp => resp.json())
+        .then(json => {
+            for (const score of json) {
+                highScore(score);
+                scores.push(score);
+            }
+        })
+}
+function sendScore(score) {
+    return fetch("./db.json", {
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify(score),
+    })
+    .then(r => r.json()) //can I just do nothing?
+    .then(obj, console.log("High scores updated: congratulations!"))
+    .catch(err => console.log("update failed: ", JSON.stringify(err.message)));
+}

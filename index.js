f7a17d05b0dfee40802b8f412abcaca6fa6c1b91
MonsterBasket <blazza1@hotmail.com>
2022-08-06 15:28:35 +1000

    I think I'm done?  (well, I can always polish more)

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //#region-----list of features-----
-//1. choose a theme
+//1. choose a theme - COMPLETE
 // - after choosing a theme, it will load a random background image from that theme 
 // - from unsplash, and a list of words related to that theme
 
-//2. playing the game
+//2. playing the game - COMPLETE
 // - words will slowly start floating from right to left from the selected word list
 // - starting slowly, and shorter words (3-5 letters).
 // - as time progresses, words will appear faster and be longer in length
@@ -12,15 +12,15 @@
 // - additional points for correct letters, minus points for missing letters?
 // - record accuracy (total correct / total typed) and longest streak
 
-//4. high scores (optional)
+//4. high scores (optional) - COMPLETE (local server)
 // - save top scores to a server, just enter your name when you finish a round and make the top 10
 
-//5. technical
+//5. technical - COMPLETE
 // - handling specific event listeners is key.  Just copy Ztype's approach: when the first letter of
 // - a word is pressed that locks the user onto that word until the word is complete - it may be as simple
 // - as saving a reference to that word into a "target" variable?
 
-//6. Next level
+//6. Next level - partially complete; no change of themes between rounds.
 // - Every 3? levels the user can pick a new theme.  Three options will float around in the middle of the screen
 // - user can't die on this stage (game is essentially paused) and there is no scoring.  Type the name of the
 // - theme to choose, this will trigger a new background image and word list.
@@ -38,18 +38,28 @@ let GC = {
     error: document.querySelector("#error"),
     input: document.querySelector("#input"),
     backgroundCover: document.querySelector("#backgroundCover"),
+    gameOver: document.querySelector("#gameOver"),
+    scores: document.querySelector("#scores"),
+    roundDiv: document.querySelector("#roundDiv"),
+    scoreSpan: document.querySelector("#scoreSpan"),
+    accuracySpan: document.querySelector("#accuracySpan"),
+    streakSpan: document.querySelector("#streakSpan"),
+    wordsSpan: document.querySelector("#wordsSpan"),
     words: [],
     difficulty: 0,
     target: {},
-    keyCount: 0,
+    keyCount: [0, 0],
     streak: 0,
     longStreak: 0,
+    highScores: [],
     score: 0,
     currentWords: "",
     enemyWords: {},
     playing: false,
     wordCount: 0,
-    lastWord: false 
+    totalWords: 0,
+    lastWord: false,
+    typeLock: false
 }
 const backupWords = ['ant', 'box', 'car', 'dog', 'egg', 'fog', 'gin', 'hot', 'ice', 'jam', 'kin', 'lie', 'map', 'nil', 'off', 'pet', 'qin', 'red', 'sly', 'tee', 'urn', 'vat', 'why', 'you', 'zen',
     'atom', 'bare', 'cave', 'dire', 'epic', 'fate', 'goal', 'heat', 'iron', 'joke', 'kept', 'list', 'made', 'note', 'ouch', 'play', 'quit', 'rest', 'sell', 'told', 'unit', 'volt', 'wind', 'xray', 'yarn', 'zeus',
@@ -106,7 +116,6 @@ function loaded() {
 function noImageFound() {
     //brief pop-up to explain why background is abstract and doesn't match typed theme
 }
-
 function newWords(theme) {
     GC.words = [];
     Promise.all([fetch(`https://api.datamuse.com/words?rel_jja=${theme}&max=200`),
@@ -144,7 +153,6 @@ function noWordsFound() {
         }, 500)
     }
 }
-
 function startGame(theme) {
     GC.welcome.className = "hidden";
     GC.playing = true;
@@ -152,25 +160,29 @@ function startGame(theme) {
     GC.longStreak = 0;
     GC.difficulty = 0;
     GC.currentWords = "";
-    GC.keyCount = 0;
+    GC.keyCount = [0,0];
     GC.score = 0;
-    GC.input.innerHTML = "";
+    GC.totalWords = 0;
+    GC.input.value = "";
     setRule();
     newBackground(theme);
     newWords(theme);
-    newRound(1000);
+    setTimeout(newRound, 1000);
 }
 function setRule(){
     GC.rule.className = getOrientation() === 'landscape' ? "verticalRule" : "horizontalRule";
     setTimeout(a=> GC.rule.classList.add(GC.rule.className === "verticalRule" ? "verticalRuleExpand" : "horizontalRuleExpand"), 200);
 }
-function newRound(displayDelay = 0){
+function newRound(){
     GC.difficulty++;
     GC.wordCount = 0;
-    GC.lastWord = false;
-    setTimeout(a => GC.roundInfo.className = "", displayDelay);
+    GC.roundInfo.className = "";
     GC.roundInfo.innerText = "Round "+GC.difficulty;
-    setTimeout(a => GC.roundInfo.className = "hidden", 3000+displayDelay)
+    setTimeout(a => {
+        GC.roundInfo.className = "hidden";
+        GC.lastWord = false;
+        
+    }, 2500)
     setTimeout(function loop() {
         const rand = Math.round(Math.random() * (1000)) + 1600 - GC.difficulty * 100; // 1 word every 1.5 seconds, +/- 0.5 seconds.  Base time decreases 100ms for each level of difficulty (that adds up fast!)
         if (!GC.lastWord && GC.playing){ //stop making words after the last word - see first line of createWord.
@@ -179,10 +191,18 @@ function newRound(displayDelay = 0){
                 loop();  
             }, rand);
         }
-    }, 4000 - displayDelay);
+    }, 3000);
 }
 function gameOver(){
-    GC.welcome.className = "";
+    GC.typeLock = true;
+    setTimeout(a=> GC.typeLock = false, 2000) //lock out typing for 2 sec.
+    loadScores();
+    GC.roundDiv.innerText = `You made it to round ${GC.difficulty}!`;
+    GC.scoreSpan.innerText = GC.score;
+    GC.accuracySpan.innerText = (Math.round((GC.keyCount[1]/GC.keyCount[0] * 100) * 100) / 100 || 0)+"%";
+    GC.streakSpan.innerText = (GC.longStreak || GC.streak);
+    GC.wordsSpan.innerText = GC.wordCount;
+    GC.gameOver.className = ""; //put a timeout on this once I implement explode animation
     GC.rule.className = "hidden";
     GC.playing = false;
     for (const key in GC.enemyWords) {
@@ -199,7 +219,7 @@ class Word {
 }
 function createWord() {
     GC.wordCount++;
-    if (GC.wordCount >= 5 + GC.difficulty * 3){
+    if (GC.wordCount >= 5 + GC.difficulty * 2){
         GC.lastWord = true;
     }
     let wordOptions = GC.words.filter(a => a.length > GC.difficulty - 3 && a.length <= GC.difficulty + 3) //reduce word list to words of appropriate length for current difficulty
@@ -241,16 +261,19 @@ function createWord() {
 }
 function typing(e) {
     // ------------- testing purposes only
-    if (e.key.match(/[1-9]/)) GC.difficulty = e.key;
+    //if (e.key >= 49 && e.key <= 57) GC.difficulty = e.key;
     // ---------------------------------------
+    if (GC.typeLock) return
+    if(e.keyCode == 32) {
+        e.preventDefault();
+      }
     if (GC.playing){
-        GC.keyCount++;
+        GC.keyCount[0]++;
         let key = (a => {
             return e.key.length === 1 && e.key.match(/[a-z A-Z_-]/i) ? e.key.toLowerCase() : ''
         })()
         if (!key) return //if non-letter typed, exit function.
         if (!GC.target.word) {  // check if Game Controller has a current target
-            // GC.target = (document.querySelector(`#${key}`) || {}) // assign a word on the screen with the matching first letter as target, or do nothing.
             GC.target = (GC.enemyWords[key] || {}) // assign a word on the screen with the matching first letter as target, or do nothing.
         }
         // ------- if above failed, subtract score ----------(note this is a sequential if, not an else)
@@ -261,6 +284,7 @@ function typing(e) {
             if (key === GC.target.childR.innerText.charAt(0)) {
                 GC.score++;
                 GC.streak++;
+                GC.keyCount[1]++;
                 GC.target.childL.innerText += GC.target.childR.innerText.charAt(0);
                 GC.target.childR.innerText = GC.target.childR.innerText.slice(1);
                 GC.target.childR.className = "untypedLetters";
@@ -270,27 +294,68 @@ function typing(e) {
                 scoreDown();
             }
             //-----------
-            if (GC.target.childR.innerText.length === 0) {
+            if (GC.target.childR.innerText.length === 0) { //word is fully typed
+                GC.totalWords ++;
                 GC.currentWords = GC.currentWords.replace(GC.target.childL.innerText.charAt(0), "")
                 GC.target.childR.className = "";
                 GC.target.childL.className = "postLetters";
                 explode(GC.target);
                 if (GC.currentWords === "" && GC.lastWord){
-                    setTimeout(newRound(), 1000);
+                    setTimeout(newRound, 1000);
                 }
             }
         }
     }
-    else{ //this else statement applies to theme selection menu
-        let key = (a => {
-            return e.key.length === 1 && e.key.match(/[a-zA-Z_-]/i) ? e.key.toLowerCase() : ''
-        })()
-        GC.input.innerHTML += key;
-        if (e.keyCode === 13) { //enter
-            startGame(GC.input.innerText);
+    else{ //this else statement applies to menus
+        if(GC.difficulty > 0) // GAMEOVER menu (difficulty is set to 1 as the first round starts, and not reset to 0 until we're done here.)
+        {
+            let key = e.key.length === 1 ? e.key : '';
+            let letter = e.key.length === 1 && e.key.match(/[a-z 0-9A-Z_-]/i) ? true : false;
+            if (GC.myName[1].innerHTML === "ENTER YOUR NAME" && letter){
+                GC.myName[1].innerHTML = key; //overwrite "ENTER YOUR NAME" with first valid keystroke
+            }
+            else{
+                GC.myName[1].innerHTML += key; //just typing
+                if (GC.myName[1].scrollWidth > GC.myName[1].clientWidth){
+                    GC.myName[1].innerHTML = GC.myName[1].innerHTML.substring(0, GC.myName[1].innerHTML.length-1);
+                }
+            }
+            if (GC.myName[0] === 0){ // next keypress after entering name reverts to welcome message
+                GC.typeLock = true;
+                setTimeout(a=> GC.typeLock = false, 1000);
+                GC.gameOver.className = "hidden";
+                GC.welcome.className = "";
+                GC.difficulty = 0;
+                return
+            }
+            if (e.keyCode === 13) { //keycode 13 is enter
+                if (GC.myName[1].innerHTML !== "ENTER YOUR NAME" && GC.myName[1].innerHTML !== ""){ //submit name if not blank or default
+                    GC.typeLock = true;
+                    const trimmed = {"id": Date.now(),
+                                     "name": GC.myName[1].innerText.trim(),
+                                     "score": GC.score}
+                    sendScore(trimmed);
+                    GC.myName[1].className = "";
+                    GC.myName[0] = 0;
+                    setTimeout(a=> GC.typeLock = false, 1000) //lock out typing for 1 sec.
+                }
+                else alert("I said enter your name!");
+            }
+            if (e.keyCode === 8) { //keycode 8 is backspace... this enables backspace... to... backspace...
+                GC.myName[1].innerHTML = GC.myName[1].innerHTML.substring(0, GC.myName[1].innerHTML.length-1);
+            }   
         }
-        if (e.keyCode === 8) { //backspace
-            GC.input.innerHTML = GC.input.innerHTML.substring(0, GC.input.innerHTML.length-1);
+        else{ // WELCOME menu
+            let key = (a => {
+                return e.key.length === 1 && e.key.match(/[a-zA-Z_-]/i) ? e.key.toLowerCase() : ''
+            })()
+            GC.input.value += key;
+            if (e.keyCode === 13) { //enter
+                startGame(GC.input.value);
+            }
+            if (e.keyCode === 8) { //backspace
+                GC.input.value = GC.input.value.substring(0, GC.input.value.length-1);
+            }
         }
     }
 }
@@ -326,7 +391,6 @@ function move(word) {
         //add left and right animation here
     }
 }
-
 function explode(target) {
     //kill the completed word
     //idea: separate each letter into it's own object
@@ -348,31 +412,43 @@ function scoreDown() {
     GC.score--;
 }
 function loadScores() {
-    return fetch("./db.json")
+    return fetch("https://monsterbasket.github.io/TypingGame/db.json/scores")
         .then(resp => resp.json())
         .then(json => {
+            GC.highScores = [];
             for (const score of json) {
-                GC.scores.push(score);
+                GC.highScores.push(score);
             }
+            GC.highScores.push({name: "ENTER YOUR NAME", score: GC.score})
+            GC.highScores.sort((a, b) => {
+                if (parseInt(a.score) < parseInt(b.score)) return 1;
+                if (parseInt(a.score) > parseInt(b.score)) return -1;
+                return 0;
+            });
+            const index = GC.highScores.findIndex(a => a.name === "ENTER YOUR NAME");
+            GC.scores.innerHTML = "";
+            for (let i = 0; i < GC.highScores.length; i++) {
+                if (i === index){
+                    GC.scores.innerHTML += `<div><span class="scoreInput">ENTER YOUR NAME</span> - <span>${GC.highScores[i].score}</span></div>`
+                }
+                else{
+                    GC.scores.innerHTML += `<div><span>${GC.highScores[i].name}</span> - <span>${GC.highScores[i].score}</span></div>`
+                }
+            }
+            GC.myName = [1,document.querySelector(".scoreInput")]; 
+            GC.scores.scrollTo(0,GC.myName[1].offsetTop - GC.scores.offsetTop - GC.scores.offsetHeight - 10); 
         })
 }
-function highScore(score) {
-    const name = "";//get player name
-    //sort list
-    sendScore({ name, score })
-
-}
-
 function sendScore(score) {
-    return fetch("./db.json", {
+    return fetch("https://monsterbasket.github.io/TypingGame/db.json/scores", {
+        method: "POST",
         headers: {
             "Content-Type": "application/json",
             "Accept": "application/json"
         },
-        method: "POST",
-        body: JSON.stringify(score),
+        body: JSON.stringify(score)
     })
-        .then(r => r.json()) //can I just do nothing?
-        .then(obj, console.log("High scores updated: congratulations!"))
+        .then(r => {}) //can I just do nothing?
+        .then(obj => {   })
         .catch(err => console.log("update failed: ", JSON.stringify(err.message)));
 }
